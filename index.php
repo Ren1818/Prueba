7b90905739231f12a23dd8a88c9573b721017435
Ren1818 <ivan.quinonez.morales@utelvt.edu.ec>
2023-04-30 14:34:10 -0500

    actualizacion 5

--- index.php
+++ index.php
@@ -80,8 +80,8 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Stalin"; 
-document.getElementById("foto").src="./images/steve.jpg";
+ document.title="Ivan"; 
+document.getElementById("foto").src="./images/ivan.jpeg";
 document.getElementById("elnombre").innerHTML="Ivan Quiñonez";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
