8e7f7632df91130410f8985e5067dbdd42332143
Ren1818 <ivan.quinonez.morales@utelvt.edu.ec>
2023-04-30 21:54:46 -0500

    actualizacion 5

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include("marco-right.php");
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
                 Posteado por:  Ivan Quiñonez<br>
-                Fecha: 2021-08-17 <br>
+                Fecha: 2023-04-30 <br>
           </div>
         </div>
     </div>
